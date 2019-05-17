d4cf4feb3cfe6830370cd0c5fcf1919978cdd993
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-16 18:50:57 -0700

    create bootstrap and supervisor class

--- constraints.py
+++
@@ -1,9 +0,0 @@
-class Constrains:
-    None
-    # TODO: define const table
-    # message size
-    # deadrop slot count (and ranges)
-    # deadrop index format
-    # padding rules
-
-    # FIXME: modulize?

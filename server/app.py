e5280c6ffaa808f657ff99bd222ebd65eaa97c80
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-18 16:47:37 -0700

    functional deaddrop

--- server/app.py
+++ server/app.py
@@ -5,11 +5,22 @@
 from flask import Flask, Response, request, redirect, jsonify
 from http import HTTPStatus
 import sys
+from packet import Packet
 
 app = Flask(__name__)
 
-round_packages = {} # lookup for current round info: temporary DB. TODO: mySQL?
+# hacky.db
+round_packets = {} # (id: request)
+deaddrops = {}
+
 clients = {} # TODO: fill via http query to dispatch
+
+# holds the tuples of the server chain crypt keys (public, private)
+# ordered from origin to deaddrop (TODO: bettter phrasing?)
+keys = [("foo", "bar"), ("titi", "toto"), ("biz", "baz")]
+
+# holds the seed used to shuffle  
+reorder = None
 
 @app.route("/")
 def hello():
@@ -17,27 +28,35 @@ def hello():
 
 @app.route('/submission/<int:id>', methods=['POST'])
 def post_submission(id):
-    recieved = request.get_json()
+    packet = Packet(request.get_json(), True)
 
-    # TODO: add validation to each
-    collect, message, drop = recieved.get("collect"), recieved.get("message"), recieved.get("drop")
-    if not (collect and message and drop):
-        results = {
-            "collect": collect, # key for requested deaddrop access
-            "drop": drop, # key for requested deaddrop deposit
-            "message": message, # message to be deposited
-        }
-        return jsonify("Incomplete submission", results), HTTPStatus.BAD_REQUEST
+    packet.decrypt("foo")
+    packet.unwrap()
+    round_packets[id] = packet
+    process(packet)
 
-    round_packages[id] = message
+    # # TODO: add validation to each
+    # collect, message, drop = recieved.get("collect"), recieved.get("message"), recieved.get("drop")
+    # if not (collect and message and drop):
+    #     results = {
+    #         "collect": collect, # key for requested deaddrop access
+    #         "drop": drop, # key for requested deaddrop deposit
+    #         "message": message, # message to be deposited
+    #     }
+    #     return jsonify("Incomplete submission", results), HTTPStatus.BAD_REQUEST
+
     return redirect(f"/deaddrop/{id}")
+
+def process(packet):
+    deaddrops[packet.drop] = packet.message
 
 @app.route('/deaddrop/<int:id>', methods=['GET'])
 def get_round(id):
+    assert(id in round_packets)
     response = { 
-        "id": id,
-        "message": round_packages.get(id),
+        "collected": deaddrops.get(round_packets[id].collect)
     }
+
     return jsonify(response)
 
 # if the current round is complete 
--- server/packet.py
+++ server/packet.py
@@ -5,9 +5,9 @@ class Packet:
     size = None # standardized size for packets
 
     def __init__(self, packet, terminal=False):
-        self.validate()
-
         self.packet = packet
+        Packet.validate_size(self.packet)
+
         self.encrypted = None
         self.decrypted = None
         
@@ -16,8 +16,9 @@ class Packet:
         self.drop = None
         self.message = None
 
-    # takes decrypted value as inclear json and fill instance with its values
+    # takes decrypted value as en clair json and fill instance with its values
     # discards any extraneous values
+    # throws error if any not found (invalid)
     def unwrap(self):
         if not self.terminal:
             raise ValueError("terminal flag not up")
@@ -28,26 +29,36 @@ class Packet:
         self.drop = self.packet.get("drop")
         self.message = self.packet.get("message")
 
+        if not (self.collect and self.drop and self.message):
+            raise ValueError("missing key for unwrap")
+
     # use key to store a decrypted version
     def decrypt(self, key):
         pass
         # TODO: use key to decrypt packet
         self.decrypted = self.packet
-        raise NotImplementedError()
+
+        # TODO: enable with crypto
+        # Packet.validate_size(self.decrypted)
+        # raise NotImplementedError()
 
     # use key to store an encrypted version
     def encrypt(self, key):
         pass
         # TODO: use key to encrypt packet
         self.encrypted = self.packet
+        Packet.validate_size(self.encrypted)
         raise NotImplementedError()
 
-    def validate(self):
-        self.validate_size()
-        raise NotImplementedError()
+    # def validate(self):
+    #     self.validate_size()
+    #     raise NotImplementedError()
 
-    def validate_size(self):
-        if len(self.packet) != self.size:
-            raise ValueError("size of packet is incorrect")
-
-        raise NotImplementedError()
+    # checks passed object against size constraint
+    @staticmethod
+    def validate_size(packet):
+        pass
+        # if len(packet) != Packet.size: # TODO: enable
+        #     raise ValueError("size of packet is incorrect")
+        # 
+        # raise NotImplementedError()

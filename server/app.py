d7c570f8301f896b0c8003e2fd9af12613508573
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-17 21:16:08 -0700

    dumb interface

--- server/app.py
+++ server/app.py
@@ -1,15 +1,53 @@
+#! /usr/bin/env python3
 
 # app.py
 
-from flask import Flask
+from flask import Flask, Response, request, redirect, jsonify
+from http import HTTPStatus
 import sys
 
 app = Flask(__name__)
 
+round_packages = {} # lookup for current round info: temporary DB. TODO: mySQL?
+clients = {} # TODO: fill via http query to dispatch
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello from the server!"
+
+@app.route('/submission/<int:id>', methods=['POST'])
+def post_submission(id):
+    recieved = request.get_json()
+
+    # TODO: add validation to each
+    collect, message, drop = recieved.get("collect"), recieved.get("message"), recieved.get("drop")
+    if not (collect and message and drop):
+        results = {
+            "collect": collect,
+            "message": message,
+            "drop": drop,
+        }
+        return jsonify("Incomplete submission", results), HTTPStatus.BAD_REQUEST
+
+    round_packages[id] = message
+    return redirect(f"/deaddrop/{id}")
+
+@app.route('/deaddrop/<int:id>', methods=['GET'])
+def get_round(id):
+    response = { 
+        "id": id,
+        "message": round_packages.get(id),
+    }
+    return jsonify(response)
+
+# if the current round is complete 
+def all_recieved():
+    raise NotImplementedError()
 
 if __name__ == '__main__':
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5001
+    port = 5001
+    try:
+        port = int(sys.argv[1])
+    except (ValueError, IndexError):
+        pass
     app.run(debug=True, port=port)

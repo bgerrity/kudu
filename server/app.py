3019acf00c43357c26b008eb181ba376c8d1e5cc
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-21 14:00:50 -0700

    implemented rounds

--- server/app.py
+++ server/app.py
@@ -3,74 +3,80 @@
 # app.py
 
 from flask import Flask, Response, request, redirect, jsonify
+
+from sys import argv
 from http import HTTPStatus
-import sys
+from collections import deque
+from threading import Lock
+
+from server import Server # hacky.db interface
 from packet import Packet
 
 app = Flask(__name__)
 
-# hacky.db
-round_packets = {} # (id: request)
-deaddrops = {}
+dispatch_port = None
 
-clients = {} # TODO: fill via http query to dispatch
+server_lock = Lock() # general purpose lock
+
+db = Server(2)
 
 # holds the tuples of the server chain crypt keys (public, private)
 # ordered from origin to deaddrop (TODO: bettter phrasing?)
 chain_keys = [("foo", "bar"), ("titi", "toto"), ("biz", "baz")]
 
-# holds the seed used to shuffle  
-reorder = None
-
-current_round = 0
-
 @app.route("/")
 def hello():
     return "Hello from the server!"
 
+# stores uploaded packet
 @app.route('/submission/<int:id>', methods=['POST'])
 def post_submission(id):
-    packet = Packet(request.get_json(), True)
+    packet = Packet(request.get_json(), terminal=True)
     packet.decrypt("foo")
 
-    try:
-        packet.unwrap()
-    except ValueError as e:
-        return f"Incomplete submission: {e}", HTTPStatus.BAD_REQUEST
+    with server_lock:
+        if db.receive_mode:
+            db.collect_request(id, packet)
+        else:
+            return f"Server not recieving", HTTPStatus.BAD_REQUEST
+
+    # try:
+    #     packet.unwrap()
+    # except ValueError as e:
+    #     return f"Incomplete submission: {e}", HTTPStatus.BAD_REQUEST
     
-    round_packets[id] = packet
-    make_drop(packet)
+    # db.packets[id] = packet
+    # make_drop(packet)
 
-    advance_round()
+    # return redirect(f"/deaddrop/{id}")
+    return f"Received POST from {id}", HTTPStatus.ACCEPTED
 
-    return redirect(f"/deaddrop/{id}")
+@app.route('/current_round', methods=['GET'])
+def get_current_round():
+    with server_lock:
+        round = db.current_round
+    return f"{round}", HTTPStatus.OK
 
-    # # TODO: add validation to each
-    # collect, message, drop = recieved.get("collect"), recieved.get("message"), recieved.get("drop")
-    # if not (collect and message and drop):
-    #     results = {
-    #         "collect": collect, # key for requested deaddrop access
-    #         "drop": drop, # key for requested deaddrop deposit
-    #         "message": message, # message to be deposited
-    #     }
-    #     return jsonify("Incomplete submission", results), HTTPStatus.BAD_REQUEST
 
 # processes the passed cleartext packet: stores its contained message with the
 # appropriate key in the deaddrop space
-def make_drop(packet):
-    deaddrops[packet.drop] = packet.message
+# def make_drop(packet):
+#     deaddrops[packet.drop] = packet.message
 
 # given an id, services its requested drop
-def collect_drop():
-    deaddrops.get(round_packets[id].collect)
+def collect_drop(id):
+        return db.return_request(id)
 
 @app.route('/deaddrop/<int:id>', methods=['GET'])
-def get_round(id):
-    assert(id in round_packets)
-    response = { 
-        "round": current_round,
-        "collected": collect_drop(id)
-    }
+def get_response(id):
+    try:
+        with server_lock:
+            response = { 
+                "round": db.current_round,
+                "collected": collect_drop(id)
+            }
+    except ValueError:
+        return f"Server not distributing", HTTPStatus.BAD_REQUEST
 
     return jsonify(response)
 
@@ -80,8 +86,15 @@ def all_recieved():
 
 if __name__ == '__main__':
     port = 5001
+    dispatch_port = 5000
+
     try:
-        port = int(sys.argv[1])
+        port = int(argv[1])
     except (ValueError, IndexError):
         pass
+    try:
+        dispatch_port = int(argv[2])
+    except (ValueError, IndexError):
+        pass
+
     app.run(debug=True, port=port)
---
+++ server/server.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+
+# server.py
+# state interface class for server: implemented as webserver process state,
+# could be attached to database for RESTful
+
+import json
+from collections import namedtuple
+
+# from bundle import Bundle
+from packet import Packet
+
+Key_Pair = namedtuple("keys", ["public", "private"])
+
+class Server:
+    def __init__(self, client_count=0):
+        self.current_round = -1
+        # self.bundle = None
+        self.client_count = client_count
+        self.key_pair = Key_Pair("foo", "bar")
+
+        self.receive_mode = True
+        self.distribute_mode = False
+
+        self.current_round = 0
+        self.deaddrops = {}
+        self.packets = {}
+
+    # # TODO: implement
+    # # indicates if server is in recieve mode
+    # def accepting_requests(self):
+    #     return self.receive_mode
+
+    def reset(self):
+        self.receive_mode = True
+        self.distribute_mode = False
+
+        self.current_round += 1
+        self.deaddrops = {}
+        self.packets = {}
+
+    def collect_request(self, id, packet):
+        if not self.receive_mode:
+            raise ValueError("not ready to recieve")
+
+        self.packets[id] = packet
+
+        if len(self.packets.keys()) == self.client_count: # enter processing mode
+            self.receive_mode = False
+            self.process_requests()
+            self.distribute_mode = True
+    
+    def process_requests(self):
+        # process drops in
+        for id, packet in self.packets.items():
+            packet.decrypt_and_process("bar")
+            self.deaddrops[packet.contents.drop] = packet.contents.message
+
+    def return_request(self, id):
+        if not self.distribute_mode:
+            raise ValueError("not ready to distribute")
+        elif id not in self.packets:
+            raise ValueError("no such id was recieved")
+        
+        requested_drop = self.packets[id].contents.collect
+        result = self.deaddrops.pop(requested_drop)
+
+
+        if len(self.deaddrops) == 0: # exit distribute mode and return to recieving
+            self.reset()
+
+        return result
+
+
+
+

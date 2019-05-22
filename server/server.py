5406fee1f52809e4ac364859f7e7a9de6645eeee
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-21 23:04:52 -0700

    encryption tested

--- server/server.py
+++ server/server.py
@@ -34,16 +34,17 @@ class Server:
 
     @staticmethod
     def gen_key_pair():
-        with open("fixed-key.pem", "rb") as f:
+        # TODO: return to generation when dispathc enabled
+        with open("server/fixed-key.pem", "rb") as f:
             server_key = RSA.import_key(f.read())
-
         # server_key = RSA.generate(2048)
-        return Key_Pair(server_key.export_key(), server_key.publickey().export_key())
+        return Key_Pair(server_key.publickey().export_key(), server_key.export_key())
 
     def reset(self):
         self.current_round += 1
         self.deaddrops = {}
         self.packets = {}
+        self.mode = Server.Modes.RECEIVING
 
     # handles
     def collect_request(self, id, packet):
@@ -64,15 +65,16 @@ class Server:
 
         # process drops in
         for _, packet in self.packets.items():
-            packet.decrypt_and_process(Key_Pair.private)
+            packet.decrypt_and_process(self.key_pair.private)
             self.deaddrops[packet.contents.drop] = packet.contents.message
-        # finished processing
+
+        self.mode = Server.Modes.DISTRIBUTING
 
     def return_request(self, id):
         if self.mode != Server.Modes.DISTRIBUTING:
             raise ValueError("not ready to distribute")
         elif id not in self.packets:
-            raise ValueError("no such id was recieved")
+            raise ValueError("no such id was received")
 
         requested_drop = self.packets[id].contents.collect
         result = self.deaddrops.pop(requested_drop)

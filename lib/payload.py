9f4ad1564caca55482dffc0b85392b65640e18d6
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-26 17:35:46 -0700

    standard formatting of message payload
    * add payload (message format) to lib and a test
    
    * improve payload format

---
+++ lib/payload.py
@@ -0,0 +1,44 @@
+import io
+from collections import namedtuple
+
+# a standardized format for the 3-tuple used to interact with Vuvuzela servers and clients
+Payload = namedtuple("payload", ["collect", "drop", "message"])
+
+ADDRESS_SIZE = 256 # size of shared secret used by
+MESSAGE_SIZE = 512 # arbitrary size
+
+def export_payload(payload):
+    """
+    Given a payload object, generates a binary instance, validating the size of each attribute.
+    Note all fields in payload must be bytes.
+    """
+
+    if not isinstance(payload, Payload):
+        raise ValueError("payload must be of type payload.Payload")
+    elif not (isinstance(payload.collect, bytes) and len(payload.collect) == ADDRESS_SIZE):
+        raise ValueError(f"collect must be bytes and length {ADDRESS_SIZE}")
+    elif not (isinstance(payload.drop, bytes) and len(payload.drop) == ADDRESS_SIZE):
+        raise ValueError(f"drop must be bytes and length {ADDRESS_SIZE}")
+    elif not (isinstance(payload.message, bytes) and len(payload.message) == MESSAGE_SIZE):
+        raise ValueError(f"drop must be bytes and length {MESSAGE_SIZE}")
+
+    return b"".join((payload.collect, payload.drop, payload.message))
+
+
+def import_payload(data):
+    """
+    Given bytes data, generates a Payload instance, validating the size.
+    Note all fields in payload must be bytes.
+    """
+    if not isinstance(data, bytes):
+        raise TypeError(f"data must be bytes")
+    if len(data) != (ADDRESS_SIZE + ADDRESS_SIZE + MESSAGE_SIZE):
+        raise ValueError(f"data is incorrect size: \
+            expected sum(addr:{ADDRESS_SIZE}, addr:{ADDRESS_SIZE}, msg:{MESSAGE_SIZE}) got {len(data)}")
+ 
+    string_in = io.BytesIO(data) # process data string as file
+
+    collect, drop, message = \
+        [ string_in.read(x) for x in (ADDRESS_SIZE, ADDRESS_SIZE, MESSAGE_SIZE) ]
+
+    return Payload(collect, drop, message)
---
+++ test/test_payload.py
@@ -0,0 +1,20 @@
+import sys, os
+
+sys.path.append(os.path.abspath('../Kudu'))
+
+from lib import payload as pl
+
+# local changes
+pl.ADDRESS_SIZE = 3
+pl.MESSAGE_SIZE = 10
+
+pay = pl.Payload("foo".encode(), "bar".encode(), "saltywater".encode())
+print(pay)
+
+expor = pl.export_payload(pay)
+print(expor)
+
+impor = pl.import_payload(expor)
+print(impor)
+
+

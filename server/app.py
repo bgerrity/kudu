96d4eb9a79543dfed4f9f3dcedf09aa3c4f6b787
Brendan Gerrity <bgerrity@ucdavis.edu>
2019-05-28 15:55:27 -0700

    server prelim
    * hello world
    
    * dumb interface
    
    * packet tools
    
    * fix ordering and add notation
    
    * functional deaddrop
    
    * re-add error handling code to post
    
    * fix missing decrypt
    
    * add bundle class
    
    * clariified name
    
    * fixed prep tool
    
    * fix packet for bundle
    
    * begin implementation of rounds
    
    * implemented rounds
    
    * cleaner
    
    * prelim crypto
    
    * encryption tested
    
    * test client post script
    
    * key pair tests
    
    * add doc strings to crypto functions
    
    * create crypto lib
    
    * add aes key size param
    
    * test script for onion ops
    
    * onion packet
    
    * trivial fixes for server -- untested
    
    * server key improvement
    
    * fix recursive onionizer
    
    * improve packet test
    
    * symm enc basic lib test
    
    * packet encryption functions
    
    * testing extended
    
    * fixup
    
    * fix bundle init default
    
    * preserve degeneralized classes
    
    * server limited packet and test
    
    * consolidate client-server packet tests
    
    * packets complete and tested
    
    * complete bundle and merge server files
    
    * add bundle test
    
    * remove unused test
    
    * add client helper
    
    * move enc test to test directory
    
    * remove extraneous and reenable server
    
    * remove extraneous file
    
    * fix spacing
    
    * add payload (message format) to lib and a test
    
    * readapted server and tests
    
    * improve payload format
    
    * mildly better comments
    
    * update test
    
    * refine and test web interface
    
    * dispatch and related
    
    * prelim top-level class skeletons
    
    * fix filename case
    
    * create bootstrap and supervisor class
    
    * add list skeleton
    
    * hello world
    
    * dumb interface
    
    * packet tools
    
    * fix ordering and add notation
    
    * functional deaddrop
    
    * re-add error handling code to post
    
    * fix missing decrypt
    
    * add bundle class
    
    * clariified name
    
    * fixed prep tool
    
    * fix packet for bundle
    
    * begin implementation of rounds
    
    * dispatch launches server and client
    
    * implemented rounds
    
    * set vscode python path
    
    * cleaner
    
    * prelim crypto
    
    * encryption tested
    
    * test client post script
    
    * key pair tests
    
    * split dispatch into a process that starts clients, server, and a dispatch server that will distribute info among clients
    
    * key storage data structure and methods for publishing and retreiving keys
    
    * clients exchange kets
    
    * brendan's changes
    
    * fixed merge conflict in packet
    
    * implemented httpstatus code checking for exchanging keys
    
    * added Diffe Hellman keys to the server
    
    * diffie hellman work on client
    
    * modify dh to use bytes not int
    
    * add pycryptodome system-specific imports
    
    * fix lib headers
    
    * fix test headers
    
    * updated client
    
    * move to prevent namespace conflicts
    
    * fix whitespace
    
    * add demo messaging
    
    * remove merge files

---
+++ server/app.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+# app.py
+
+from flask import Flask, Response, request
+
+from sys import argv
+from http import HTTPStatus
+from threading import Lock
+
+from server import Server # hacky.db interface
+
+app = Flask(__name__)
+
+dispatch_port = None
+
+server_lock = Lock() # general purpose lock
+
+db = Server(2)
+
+# holds the tuples of the server chain crypt keys (public, private)
+# ordered from origin to deaddrop (TODO: bettter phrasing?)
+chain_keys = [("foo", "bar"), ("titi", "toto"), ("biz", "baz")]
+
+@app.route("/")
+def hello():
+    return "Hello from the server!"
+
+# stores uploaded packet
+@app.route('/submission/<int:id>', methods=['POST'])
+def post_submission(id):
+    with server_lock:
+        if db.mode == db.Modes.RECEIVING:
+            db.collect_request(id, request.data)
+        else:
+            return f"Server not receiving", HTTPStatus.BAD_REQUEST
+
+    return f"Received POST from {id}", HTTPStatus.ACCEPTED
+
+@app.route('/current_round', methods=['GET'])
+def get_current_round():
+    with server_lock:
+        round = db.current_round
+    return f"{round}", HTTPStatus.OK
+
+@app.route('/deaddrop/<int:id>', methods=['GET'])
+def get_response(id):
+    with server_lock:
+        if db.mode != db.Modes.DISTRIBUTING:
+            return "Server not distributing", HTTPStatus.BAD_REQUEST
+        elif id not in db._id_map:
+            return f"No request by id:{id}", HTTPStatus.BAD_REQUEST
+
+        try:
+            response = collect_drop(id)
+        except KeyError:
+            return f"Already returned id:{id}", HTTPStatus.BAD_REQUEST
+
+
+    return Response(response)
+
+# given an id, services its requested drop
+# assumes server lock is owned by thread
+def collect_drop(id):
+    return db.return_request(id)
+
+if __name__ == '__main__':
+    port = 5001
+    dispatch_port = 5000
+
+    try:
+        port = int(argv[1])
+    except (ValueError, IndexError):
+        pass
+    try:
+        dispatch_port = int(argv[2])
+    except (ValueError, IndexError):
+        pass
+
+    app.run(debug=True, port=port)

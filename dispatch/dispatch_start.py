f66951e6713c8708577c92a9aeac2486080beacc
Filip Stamenkovic <34168715+fstamenkovic@users.noreply.github.com>
2019-05-28 15:48:19 -0700

    dispatch prelim

---
+++ dispatch/dispatch_start.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import subprocess
+import time
+import sys
+
+def spin_up_dispatch_server():
+    subprocess.call(["gnome-terminal", "--", "./dispatch_server.py"])
+    time.sleep(2)
+
+def spin_up_clients():
+    subprocess.call(["gnome-terminal", "--", "./client.py", "1", "2"])
+    subprocess.call(["gnome-terminal", "--", "./client.py", "2", "1"])
+    print("started clients")
+
+def spin_up_vuvuzela_server():
+    subprocess.call(["gnome-terminal", "--", "server/server.py"])
+
+spin_up_dispatch_server()
+spin_up_clients()
+#spin_up_vuvuzela_server()
